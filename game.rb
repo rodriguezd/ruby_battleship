92e3f96e00584f9804cfab8613301902907850ae
David Rodriguez <davidrodriguez212@gmail.com>
2013-01-23 14:23:25 -0500

    Add coordinate entry error messaging

--- game.rb
+++ game.rb
@@ -29,28 +29,44 @@ class Game
 	end
 
 	def place_carrier(player)
-		position = {}
-		print "Carrier orientation: horizaontal(H) or vertical(V)? "
-		input = gets.chomp.rstrip.upcase
-		if input == 'H'
-			orientation = :horizontal
-		else
-			orientation = :vertical
-		end
-		print "Carrier starting position: "
-		input = gets.chomp.rstrip.upcase
-		position[:row] = Board::ROW.rindex(input.split(//, 2)[0])
-		position[:column] = Board::COLUMN.rindex(input.split(//, 2)[1])
+		valid = false
+		while valid == false do
+			position = {}
+			while valid == false do
+				print "Carrier orientation: horizaontal(H) or vertical(V)? "
+				input = gets.chomp.rstrip.upcase
+				if input == 'H'
+					orientation = :horizontal
+					valid = true
+				elsif input == 'V'
+					orientation = :vertical
+					valid = true
+				else
+					puts "Invalid orientation entry."
+				end
+			end
 
-		if player.board.valid_coordinates?(:carrier, position, orientation)
-			if player.board.check_clearance?(:carrier, position, orientation)
+			valid = false
+			while valid == false do
+				print "Carrier starting position: "
+				input = gets.chomp.rstrip.upcase
+				position[:row] = Board::ROW.rindex(input.split(//, 2)[0])
+				position[:column] = Board::COLUMN.rindex(input.split(//, 2)[1])
+				if (0..9).include?(position[:row]) && (0..9).include?(position[:column])
+					valid = true
+				else
+					puts "Invalid coordinates."
+				end
+			end
+
+			valid = false
+			if player.board.valid_coordinates?(:carrier, position, orientation) && player.board.check_clearance?(:carrier, position, orientation)
 				player.carrier.place_ship(player.board, position, orientation)
+				valid = true
 				@player.board.to_s
 			else
-
+				puts "Invalid position for ship."
 			end
-		else
-
 		end
 	end
 
